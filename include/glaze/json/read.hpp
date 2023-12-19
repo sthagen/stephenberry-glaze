35cfed5e04f719da79c0b5d9146b37acd64cf7a7
Stephen Berry <stephenberry.developer@gmail.com>
2023-12-19 10:59:13 -0600

    current code

--- include/glaze/json/read.hpp
+++ include/glaze/json/read.hpp
@@ -579,7 +579,65 @@ namespace glz
                }
                else {
                   if constexpr (!Opts.force_conformance) {
+                     value.clear(); // Single append on unescaped strings so overwrite opt isnt as important
                      auto start = it;
+                     
+                     auto handle_escaped = [&] {
+                        switch (*it) {
+                        case '"':
+                        case '\\':
+                        case '/':
+                           value.push_back(*it);
+                           break;
+                        case 'b':
+                           value.push_back('\b');
+                           break;
+                        case 'f':
+                           value.push_back('\f');
+                           break;
+                        case 'n':
+                           value.push_back('\n');
+                           break;
+                        case 'r':
+                           value.push_back('\r');
+                           break;
+                        case 't':
+                           value.push_back('\t');
+                           break;
+                        case 'u': {
+                           ++it;
+                           read_escaped_unicode<char>(value, ctx, it, end);
+                           return;
+                        }
+                        default: {
+                           ctx.error = error_code::invalid_escape;
+                           return;
+                        }
+                        }
+                        ++it;
+                     };
+                     
+                     while (it < end) {
+                        skip_till_escape_or_quote(ctx, it, end);
+                       if (bool(ctx.error)) [[unlikely]]
+                          return;
+
+                       if (*it == '"') {
+                          value.append(start, size_t(it - start));
+                          ++it;
+                          return;
+                       }
+                       else {
+                          value.append(start, size_t(it - start));
+                          ++it;
+                          handle_escaped();
+                          if (bool(ctx.error)) [[unlikely]]
+                             return;
+                          start = it;
+                       }
+                     }
+                     
+                     /*auto start = it;
                      
                      skip_till_unescaped_quote(ctx, it, end);
                      if (bool(ctx.error)) [[unlikely]]
@@ -610,7 +668,7 @@ namespace glz
                         return;
                      }
                      
-                     ++it;
+                     ++it;*/
                   }
                   else {
                      auto handle_escaped = [&] {
